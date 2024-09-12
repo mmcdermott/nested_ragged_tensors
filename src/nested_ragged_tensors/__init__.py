c0693d990697ce20c1a8b80694e26ee4e37370cc
Matthew McDermott <mattmcdermott8@gmail.com>
2024-09-12 12:52:35 -0400

    Removing things not needed for GH pages.

--- src/nested_ragged_tensors/__init__.py
+++
@@ -1,7 +0,0 @@
-from importlib.metadata import PackageNotFoundError, version
-
-__package_name__ = "nested_ragged_tensors"
-try:
-    __version__ = version(__package_name__)
-except PackageNotFoundError:  # pragma: no cover
-    __version__ = "unknown"
